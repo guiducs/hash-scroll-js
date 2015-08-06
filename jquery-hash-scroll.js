bb8e8bf957a1d19495154c3b305a2065b58c82a7
aguinaldo <aguinaldo.canali@gmail.com>
2015-08-06 16:58:41 -0400

    Logica atualizada

--- jquery-hash-scroll.js
+++ jquery-hash-scroll.js
@@ -1,32 +1,22 @@
-// Todo:
-// resizeTargets; 
-// coloca o tamanho dos targets com 100% da tela caso eles sejam menores.
-// se as páginas se moverem na horizontal tem que ser 100% e redimensionar de acordo se for a opção. Vertical redimensionar so se for menor que 100%
-// mode: dizer se o movimento e horizontal ou vertical
-// html5: dizer se usa a hash ou pushState
-// manter a hash inteira na url se o target for o que estiver lá
-// só ativar a pagina depois da animacao ou scrolls, até lá é só o target?
-// Chamar um method um metodo pra disparar um evento antes de chegar na pagina (onBeforePageActive, offsetBefore), stopOnOffset ta funcionando assim mas o nome e jeito estão estnhos
-// http://stackoverflow.com/questions/1034306/public-functions-from-within-a-jquery-plugin methodos publicos
 ;(function($){
 
   function hashScroll(wrapper, options) {
     this.defaults = $.extend({
       selector: '.page',
-      speed: 300,
+      speed: 1000,
       easing: 'easeInOutQuad',
       offset: 0,
       stopOnOffset: true,
       beforeMove: function() {},
       afterMove: function() {},
-      onPageActive: function() {},
-      onReachOffset: function() {}
+      onPageActive: function() {}
     }, options);
 
-		this.wrapper   = wrapper;
-		this.$targets  = null;
-		this.$active   = null;
-		this.watchHash = true;
+    this.wrapper   = wrapper;
+    this.$targets  = null;
+    this.$target   = null;
+    this.$active   = null;
+    this.watchHash = true;
     this.run();
   };
 
@@ -34,7 +24,6 @@
     run: function(options) {
       this.getTargets();
       this.bindUI();
-      this.activeByHash();
     }, 
     getTargets: function() {
       if(this.$targets == null) {
@@ -65,103 +54,105 @@
 
       return null;
     },
-    getActive: function() {
-      if(this.$active == null) {
-        return this.getTargets()[0];
-      }
-      return this.$active;
-    },
     getPrev: function() {
-      var $prev = this.getActive().prevAll(this.defaults.selector).first();
-      
-      if($prev.length) {
-        return $prev;
+      if(this.$active == null) {
+        return this.getTargets()[0].prevAll(this.defaults.selector).first();
       }
-
-      return this.getActive();
+      return this.$active.prevAll(this.defaults.selector).first();  
     },
     getNext: function() {
-      var $next = this.getActive().nextAll(this.defaults.selector).first();
-      
-      if($next.length) {
-        return $next;
+      if(this.$active == null) {
+        return this.getTargets()[0].nextAll(this.defaults.selector).first();
       }
-
-      return this.getActive();
+      return this.$active.nextAll(this.defaults.selector).first();  
     },
     getHash: function() {
       var hash = window.location.hash;
-      
-      if(hash) {
+      if( hash ) {
         hash = hash.replace('!', '').split('/')[0]; 
       }
-
       return hash;
     },
-    belongsToTargets: function(hash) {   
-      return (typeof $(hash) !== "undefined" && $(hash).hasClass(this.defaults.selector.replace('.', '')));
+    isTarget: function(hash) {   
+      if( $(hash).length ) {
+        return $(hash).hasClass(this.defaults.selector.replace('.', ''));
+      }  
+      return false;
     },
-    isCurrentTargetEqualsActive: function() {
-      return this.getActive().is(this.getTarget());
-    },
-    active: function($target) {
-      this.$active = $target;
-      this.move();
+    isTargetActive: function() {
+      if(this.$active == null) {
+        return false;
+      }
+      return this.$active.is(this.$target);
     },
     activeByHash: function() {
       var hash = this.getHash();
       if( hash ) {
-        if(this.belongsToTargets(hash)) {  // É uma das páginas?
-          this.active($(hash)); // Ativa
+        if(this.isTarget(hash)) {  // É uma das páginas?
+          this.$target = $(hash);
+          this.move(this.$target); 
         }
       }
     },
-    updateHash: function() {
-      if(this.isCurrentTargetEqualsActive()) {
-        return;
-      }
-
-      if(this.getTarget() != null) {
-        this.active(this.getTarget());
-        this.defaults.onPageActive(this.$active);
-
-        if(this.watchHash) {
-          window.location.hash = "!" + this.$active.attr('id');
-        }
-      }
-    },
-    move: function() {
-      if(this.isCurrentTargetEqualsActive()) {
+    move: function($target) {
+      // A página já está ativa
+      if(this.isTargetActive()) {
         return;
       }
 
       var self = this;
       
-      // Desabilita a atualizacao da hash no scroll
+      // Desabilita a atualizacao da hash no scroll ate a animacao terminar
       self.watchHash = false; 
 
       // Chama o callback que antecede o move()
-      self.defaults.beforeMove( self.getActive() );
+      self.defaults.beforeMove( $target );
+
+      // Calcula a distancia
+      var distance = (self.defaults.stopOnOffset) ? $target.offset().top - self.defaults.offset : $target.offset().top;
 
       // Move
-      var distance = (self.defaults.stopOnOffset) ? self.getActive().offset().top - self.defaults.offset : self.getActive().offset().top;
-
       $('html,body').stop().animate({ 
         scrollTop: distance
       }, self.defaults.speed, self.defaults.easing, function() {
         // O callback do animate estava sendo executado antes da animação terminar, por isso o setTimeout
         setTimeout(function(){ 
           self.watchHash = true; // A hash volta a poder ser atualizada no scroll
-          self.defaults.afterMove( self.getActive() );
+          self.defaults.afterMove( $target );
         }, 300);
 
       });
     },
     movePrev: function() {
-      this.active(this.getPrev());
+      this.$target = this.getPrev();
+      this.move(this.$target);
     },
     moveNext: function() {
-      this.active(this.getNext());
+      this.$target = this.getNext();
+      this.move(this.$target);
+    },
+    updateHash: function() {
+      //Não está em nenhuma página
+      if(this.getTarget() == null) {
+        return;
+      }
+
+      //
+      this.$target = this.getTarget();
+
+      // A página já está ativa?
+      if(this.isTargetActive()) {
+        return;
+      }
+
+      this.$active = this.getTarget();
+      
+      this.defaults.onPageActive(this.$active);
+
+      // Só muda a url quando a animação parar
+      if(this.watchHash) {
+        window.location.hash = "!" + this.$active.attr('id');
+      }
     },
     bindUI: function() {
         
@@ -171,6 +162,8 @@
         self.activeByHash();
       }).scroll(function() {	
         self.updateHash();
+      }).load(function(){
+        self.activeByHash();
       });
 
         // Habilita navegação pelo teclado
